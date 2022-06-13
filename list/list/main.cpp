61c1c623ac6b29c8ad5981cdcb3579909b9af543
ikduck <wogur2202@gmail.com>
2022-06-13 12:52:22 +0900

    List_v0.2

--- list/list/main.cpp
+++ list/list/main.cpp
@@ -17,6 +17,8 @@ using namespace std;
 	양방향 -> front() back() 주소가 다있음
 */
 
+// 배열처럼 직접접근이 안되기 때문에 재귀함수나 iterater을 사용해서 간접접근을 해야한다.
+
 struct List
 {
 	// List* front;
@@ -24,11 +26,17 @@ struct List
 	List* Back;
 };
 
-void AddObject(List* _Next, const int& _Value);
-void Output(List* _Next);
+// erase에서 사용
+// 구조체에 적어주면 필요없음
+List* Front = nullptr;
 
-void insert(List* _Next, const int& _Value, const int& _where);
-void erase(List* _Next, const int& _where);
+// 전방선언과 같은 위치에 넣어두는게 매너임
+void push_back(List* _Next, const int& _Value);
+
+void insert(List* _Next, const int _Where, const int& _Value);
+void erase(List* _Next, const int _Where);
+
+void Output(List* _Next);
 
 int main(void)
 {
@@ -40,16 +48,21 @@ int main(void)
 	for (int i = 0; i < 10; ++i)
 	{
 		// *** 10회 추가
-		AddObject(NumberList, i * 10 + 10 );
+		push_back(NumberList, i * 10 + 10 );
 	}
 
+	insert(NumberList, 2, 15);
+	Output(NumberList);
+
+
+	erase(NumberList, 2);
 	// for (int i = 0; i < 10; ++i)
 	Output(NumberList);
 
 	return 0;
 }
 
-void AddObject(List* _Next, const int& _Value)
+void push_back(List* _Next, const int& _Value)
 {
 	if (_Next->Back == nullptr)
 	{
@@ -61,7 +74,64 @@ void AddObject(List* _Next, const int& _Value)
 	}
 	else
 	{
-		AddObject(_Next->Back, _Value);
+		push_back(_Next->Back, _Value);
+	}
+}
+
+void insert(List* _Next, const int _Where, const int& _Value)
+{
+	if (_Where == 1)
+	{
+		List* Temp = new List;
+
+		Temp->Value = _Value;
+		Temp->Back = _Next->Back;
+
+		_Next->Back = Temp;
+
+	}
+	else
+	{
+		insert(_Next->Back, _Where + (-1), _Value);
+	}
+}
+
+
+void erase(List* _Next, const int _Where)
+{
+	// 안됨 왜안되는지 모르겟음....
+	/*
+	if (_Where == 1)
+	{
+		List* Temp = _Next;
+		_Next = _Next->Back;
+
+		delete Temp;
+		Temp = nullptr;
+	}
+	*/
+	
+	if (_Where == 0)
+	{
+		// 됨 
+		/*
+		List* Temp = _Next->Back;
+		_Next->Back = _Next->Back->Back;
+
+		delete Temp;
+		Temp = nullptr;
+		*/
+	
+		Front->Back = _Next->Back;
+
+		delete _Next;
+		_Next = nullptr;
+		
+	}
+	else
+	{
+		Front = _Next;
+		erase(_Next->Back, _Where + (-1));
 	}
 }
 
@@ -74,7 +144,9 @@ void Output(List* _Next)
 	// 호출
 	if (_Next->Back != nullptr)
 		Output(_Next->Back);
-	
+
+
+
 	// 호출 후 출력 하면 오름차순
 	/*
 	// 호출
@@ -84,14 +156,4 @@ void Output(List* _Next)
 	// 출력
 	cout << _Next->Value << endl;
 	*/
-}
-
-void insert(List* _Next, const int& _Value, const int& _where)
-{
-
-}
-
-void erase(List* _Next, const int& _where)
-{
-
 }
